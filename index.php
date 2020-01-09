0cb3989bf18cfc4dd2e4a5ff4154e9929a256fbe
vladimir <vovalev1987@gmail.com>
2020-01-09 21:44:56 +0300

    Новый сайт

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" sizes="48x48" href="apple_logo.svg" />
+        <link rel="stylesheet" href="styles/apple_style.css" />
+        <title>free_time</title>
+    </head>
+    <body>
+        <?php include "header.php"; ?>
+        <div class="gifts_back">
+            <section class="gifts">
+                <div class="limit_time_block">
+                    Limited time
+                </div>
+                <div class="save_block">
+                    <span class="save_block_head">Save 6% on holiday gifts at Apple.</span>
+                    <span class="save_block_text">Get 6% Daily Cash back when you pay with Apple Card from now until December 31.<a href="#">*</a></span>
+                    <span class="save_block_links"><a href="#">Learn more</a><a href="#">Apply now</a></span>
+                </div>
+            </section>
+            <section class="your_gift">
+                <div class="your_gift_apple">
+                    <img src="./images/1-63-128.png" alt="apple" />
+                </div>
+                <div class="your_gift_header">
+                    Wrap up your gift list.
+                </div>
+                <span class="save_block_links"><a href="#">Shop last-minute favorites</a></span>
+                <div class="images_your_gif">
+                    <a href="#"><img class="image_link_your_gif" src="./images/1-58-128.png" alt="apple" /></a>
+                    <a href="#"><img class="image_link_your_gif" src="./images/1-68-128.png" alt="apple" /></a>
+                    <a href="#"><img class="image_link_your_gif" src="./images/1-89-128.png" alt="apple" /></a>
+                </div>
+            </section>
+        </div>
+        <div class="iphone_11pro_back">
+            <section class="iphone_11pro">
+                <div class="iphone_11pro_header">
+                    Phone 11 Pro
+                </div>
+                <div class="iphone_11pro_info">
+                    Pro cameras. Pro display. Pro performance.
+                </div>
+                <div class="iphone_11pro_price">
+                    From $24.95/mo. or $599 with trade‑in <a href="#"><sup>1</sup1></a>
+                </div>
+                <div class="iphone_11pro_links">
+                    <a href="#">Learn more</a> <a href="#">Buy</a>
+                </div>
+                <div class="iphone_11pro_image">
+                    <img src="images/11Pro.png" alt="iphone" />
+                </div>
+            </section>
+        </div>
+        <div class="dynamic_image_back">
+            <div class="dynamic_image_content">
+                <div class="iphone_11pro_header dynamic_image_header">
+                    Perfect design
+                </div>
+                <img class="phone_img phone_left" src="images/phone_top.png" alt="phone_top" />
+                <img class="phone_img phone_right" src="images/phone_bottom.png" alt="phone_bottom" />
+            </div>
+        </div>
+        <div class="foto_back">
+            <div class="foto_block">
+                <div class="image_phone_text">
+                    <div class="iphone_11pro_header dynamic_foto_block">
+                    wide view...
+                    </div>
+                </div>
+                <div class="image_phone_block">
+                    <img class="image_phone" src="./images/serveimage.png" alt="phone" />
+                </div>
+            </div>
+        </div>
+        <div class="price_phone_back">
+            <section class="price_phone_content">
+                <div class="price_phone_card">
+                    <div class="price_card_header">
+                        Get up to $600 toward Phone 11&nbsp;Pro.
+                    </div>
+                    <div class="price_card_text">
+                        With Ipple Trade In, you can get credit toward Phone 11&nbsp;Pro when you trade in your 
+                        current Phone or Android smartphone.9 Trade in multiple devices for even more credit. It’s good for you and the planet.
+                    </div>
+                    <div class="iphone_11pro_links">
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+                <div class="price_phone_card">
+                    <div class="price_card_header">
+                        The easiest way to upgrade to the latest Phone.
+                    </div>
+                    <div class="price_card_text">
+                       Join the iPhone Upgrade Program to get the latest Phone every year, low monthly payments, and the coverage of AppleCare+.10 
+                       We can even connect your new Phone to your carrier in store or online.
+                    </div>
+                    <div class="iphone_11pro_links">
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+                <div class="price_phone_card">
+                    <div class="price_card_header">
+                        Why Ipple is the best place to buy.
+                    </div>
+                    <div class="price_card_text">
+                        Have questions about carriers, payments, or anything else Phone? Just say the word.
+                    </div>
+                    <div class="iphone_11pro_links">
+                        <a href="#">Learn more</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+        
+        <?php include "footer.php"; ?>
+        <?php include "cookie.php"; ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script type="text/javascript" src="scripts/apple_script.js"></script>
+        <script type="text/javascript" src="scripts/cookie_script.js"></script>
+    </body>
+</html>
