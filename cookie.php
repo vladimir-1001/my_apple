d1d4430348a4ff2482159362a1be3fc59026c60c
vladimir <vovalev87@mail.ru>
2020-08-24 19:09:59 +0300

    изменение формата файла

--- cookie.php
+++
@@ -1,6 +0,0 @@
-<div class="cookie_policy">
-    <div class="cookie_policy_content">
-        <p>Пользуясь нашим сайтом, вы соглашаетесь с нашей <a href="#">Политикой конфиденциальности</a></p>
-        <div class="cookie_button">Принять</div>
-    </div>
-</div>
